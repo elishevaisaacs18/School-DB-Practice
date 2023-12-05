130f88697d6af9141f210930befa60d3b151c14a
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-12-05 10:31:47 +0200

    table creation

--- initDB.js
+++ initDB.js
@@ -1,13 +1,14 @@
-const { query } = require("express");
+// const { query } = require("express");
 const mysql = require("mysql");
 const fs = require("node:fs");
 const path = require("path");
 
 async function initDB() {
+  const queriesArr = [];
   const paths = await fs.promises.readdir(
     path.resolve(__dirname, "./entities")
   );
-  paths.forEach(async (filePath) => {
+  for (const filePath of paths) {
     const tableName = filePath.split(".")[0];
     const file = await JSON.parse(
       await fs.promises.readFile(
@@ -15,6 +16,7 @@ async function initDB() {
         "utf-8"
       )
     ).columns;
+
     let sql = "";
     file.forEach((column, index) => {
       for (const columnAttribute in column) {
@@ -30,12 +32,95 @@ async function initDB() {
         sql += ",";
       }
     });
+
     const query = `
-    DROP TABLE IF EXISTS ${tableName}
-    CREATE TABLE IF NOT EXISTS ${tableName}
-    (${sql})`;
-    console.log(query);
+      DROP TABLE IF EXISTS ${tableName};
+      CREATE TABLE IF NOT EXISTS ${tableName} (${sql});
+    `;
+
+    queriesArr.push(query.trim());
+  }
+
+  return queriesArr;
+}
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "z10mz10m",
+  database: "school",
+  multipleStatements: true,
+});
+
+function executeQuery(sql, values) {
+  con.query(sql, values, function (err, result) {
+    if (err) throw err;
+    console.log("success");
+  });
+}
+async function createTables() {
+  const tableCreationQueries = await initDB();
+  Array.from(tableCreationQueries).forEach((query) => {
+    executeQuery(query);
   });
 }
 
-initDB();
+createTables();
+
+// const createTables = `CREATE TABLE IF NOT EXISTS teacher (id INT PRIMARY KEY AUTO_INCREMENT, name TEXT);
+// CREATE TABLE IF NOT EXISTS course (id INT PRIMARY KEY AUTO_INCREMENT, name TEXT);
+// CREATE TABLE IF NOT EXISTS student (id INT PRIMARY KEY AUTO_INCREMENT, name TEXT);
+// CREATE TABLE IF NOT EXISTS student_course(student_id INT NOT NULL, course_id INT NOT NULL, PRIMARY KEY (student_id, course_id));
+// CREATE TABLE IF NOT EXISTS teacher_course(teacher_id INT NOT NULL, course_id INT NOT NULL, PRIMARY KEY (teacher_id, course_id));`;
+
+// const studentValues = [["eli"], ["eyal"], ["shir"], ["tal"], ["shoham"]];
+// const teacherValues = [
+//   ["rachel"],
+//   ["bracha"],
+//   ["shlomo"],
+//   ["shafran"],
+//   ["tehila"],
+// ];
+
+// const courseValues = [["java"], ["js"], ["c"], ["c#"], ["python"]];
+
+// const courseTeacherValues = [
+//   [1, 1],
+//   [1, 2],
+//   [2, 1],
+//   [3, 4],
+//   [2, 4],
+// ];
+
+// const courseStudentValues = [
+//   [1, 1],
+//   [1, 2],
+//   [2, 1],
+//   [3, 4],
+//   [2, 4],
+// ];
+
+// const insertStudentValues = "INSERT INTO student (name) VALUES ?;";
+// const insertTeacherValues = "INSERT INTO teacher (name) VALUES ?;";
+// const insertCourseValues = "INSERT INTO course (name) VALUES ?;";
+// const insertCourseTeacherValues = "INSERT INTO student_course VALUES ?;";
+// const insertCourseStudentValues = "INSERT INTO teacher_course VALUES ?;";
+
+// const joinTables = `SELECT course.name AS course_name, teacher.name AS teacher_name, student.name AS student_name
+// FROM course
+// INNER JOIN student_course
+// ON course.id = student_course.course_id
+// INNER JOIN student
+// ON student.id = student_course.student_id
+// INNER JOIN teacher_course
+// ON teacher_course.course_id = student_course.course_id
+// INNER JOIN teacher
+// ON teacher.id = teacher_course.teacher_id;`;
+
+// executeQuery(createTables);
+// executeQuery(insertStudentValues, [studentValues]);
+// executeQuery(insertTeacherValues, [teacherValues]);
+// executeQuery(insertCourseValues, [courseValues]);
+// executeQuery(insertCourseTeacherValues, [courseTeacherValues]);
+// executeQuery(insertCourseStudentValues, [courseStudentValues]);
+// executeQuery(joinTables);
